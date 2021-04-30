286162c123df4e88d6e628767ffaae47ca874fee
alexanderbenerink <alexander.benerink@outlook.com>
2021-05-01 01:16:53 +0200

    rewrote about

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,13 +9,13 @@
                   <div class="content has-text-light">
                     <h1 class="title is-2 has-text-weight-medium">Bio</h1>
                       <p>
-                        I am a young (web) developer from The Netherlands, who is aspiring to become a full stack developer. To the right you'll see the technologies I've worked with.
+                        I am a simple code monkey from The Netherlands who is aspiring to one day become a full stack developer in the field of software engineering. In the following section you'll see the technologies I've worked with till so far.
                       </p>
                       <p>
-                        At the moment web development speaks the most to me. But I also am interested in software and game development. Some hobbies include gaming with friends, listening to music and excercise. My peers experience me as friendly, enthusiastic and eager-to-learn. I have a hands-on learning approach (learning by doing).
+                        I like tinkering with computers, software, websites and game engines. As a career choice I'm interested in working in the field of software engineering, which includes aspects of software- and webdevelopment (from the design phase to post-production).
                       </p>
                       <p>
-                       In my spare time I like programming and I'm constantly trying to improve myself, including in all facets of life aswell.
+                       Some hobbies of mine include excercising, hanging with friends, learning how to program, read books and listening to music. My peers experience me as friendly, enthusiastic and eager-to-learn. I find that learning something new through a hands-on approach works the best for me (learning by doing). Every day I'm trying to do something new or productive to push my boundaries and improve.
                       </p>
                   </div>
                 </div>
@@ -23,7 +23,7 @@
           </div>
           <div class="column is-one-quarter"> <!-- Skill 1 card -->
             <div class="box has-background-grey-dark has-text-light">
-              <h3 class="title is-4 has-text-weight-medium">Web Development</h3>
+              <h3 class="title is-4 has-text-weight-medium">Software & Web Development</h3>
               <div class="content">
                 <p>Some technologies I've worked with include:</p>
                 <ul>
@@ -40,18 +40,12 @@
           </div>
           <div class="column is-one-quarter"> <!-- Skill 2 card -->
             <div class="box has-background-grey-dark has-text-light">
-              <h3 class="title is-4 has-text-weight-medium">Game Development</h3>
+              <h3 class="title is-4 has-text-weight-medium">Game Development/Spare time</h3>
               <div class="content">
                 <p>
                   In my spare time I like to work on a personal project inside Unreal Engine 4. This is when I get the most 'creative pleasure' out of my day, besides working on websites. This also includes modeling in Blender and UE4 itself, blueprinting, and (dabbling) in C++. 😁
                 </p>
               </div>
-            </div>
-          </div>
-          <div class="column is-one-quarter"> <!-- Skill 3 card -->
-            <div class="box has-background-grey-dark has-text-light">
-              <h3 class="title is-4 has-text-weight-medium">Software Development</h3>
-              <p>I have created several small applications inside C# but I'd like to learn more about programming in Python and C++.</p>
             </div>
           </div>
         </div>
